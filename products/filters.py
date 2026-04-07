c7e9b203c29ccab6a0bd3c59a1742a282aa33e70
mouaadh11 <mouaadhsahailia@gmail.com>
2026-04-07 17:57:23 +0200

    feat: Enhance ProductFilter to include on_sale fields

--- products/filters.py
+++ products/filters.py
@@ -3,15 +3,23 @@ from .models import Product
 
 
 class ProductFilter(django_filters.FilterSet):
-    min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
-    max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
+    min_price = django_filters.NumberFilter(
+        field_name='price', lookup_expr='gte')
+    max_price = django_filters.NumberFilter(
+        field_name='price', lookup_expr='lte')
     in_stock = django_filters.BooleanFilter(method='filter_in_stock')
+    on_sale = django_filters.BooleanFilter(method='filter_on_sale')
 
     class Meta:
         model = Product
-        fields = ['category', 'min_price', 'max_price']
+        fields = ['category']
 
     def filter_in_stock(self, queryset, name, value):
         if value:
             return queryset.filter(stock__gt=0)
         return queryset
+
+    def filter_on_sale(self, queryset, name, value):
+        if value:
+            return queryset.filter(discount_price__isnull=False)
+        return queryset.filter(discount_price__isnull=True)
