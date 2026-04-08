ca56f819e266fe2456627aebf59d6d387b869497
mouaadh11 <mouaadhsahailia@gmail.com>
2026-04-08 17:13:49 +0200

    refactor: Clean up admin.py and enhance image display styles for Product and Category models

--- products/admin.py
+++ products/admin.py
@@ -5,10 +5,6 @@ from django.contrib import admin
 from .models import Category, Product, ProductImage
 
 
-# class ProductImageInline(admin.TabularInline):
-#     model = ProductImage
-#     extra = 1
-
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
     extra = 1
@@ -26,9 +22,10 @@ class ProductImageInline(admin.TabularInline):
 
     image_preview.short_description = 'Preview'
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name','image_tag', 'price',
+    list_display = ['name', 'image_tag', 'price',
                     'stock', 'is_active', 'created_at']
     list_filter = ['is_active', 'category']
     search_fields = ['name', 'sku']
@@ -39,7 +36,7 @@ class ProductAdmin(admin.ModelAdmin):
         image = obj.images.filter(is_primary=True).first()
         if image and image.image:
             return format_html(
-                '<a href="{}" target="_blank"><img src="{}" style="height:100px;width:100px;object-fit:cover;" /></a>',
+                '<a href="{}" target="_blank"><img src="{}" style="height:150px;width:150px;object-fit:cover;border-radius:6px;" /></a>',
                 image.image.url,
                 image.image.url
             )
@@ -48,22 +45,30 @@ class ProductAdmin(admin.ModelAdmin):
     image_tag.short_description = 'Image'
 
 
-# @admin.register(Category)
-# class CategoryAdmin(admin.ModelAdmin):
-#     prepopulated_fields = {'slug': ('name',)}
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'image_tag']
     prepopulated_fields = {'slug': ('name',)}
+    readonly_fields = ['image_preview']   # 👈 important
+    fields = ['name', 'slug', 'description',
+              'image_preview', 'image']  # 👈 order
+    list_display = ['name', 'slug', 'image_tag']
 
     def image_tag(self, obj):
         if obj.image:
             return format_html(
-                '<a href="{}" target="_blank"><img src="{}" style="height:100px;width:100px;object-fit:cover;" /></a>',
+                '<a href="{}" target="_blank"><img src="{}" style="height:100px;width:100px;object-fit:cover;border-radius:6px;" /></a>',
                 obj.image.url,
                 obj.image.url
             )
         return "—"
 
-    image_tag.short_description = 'Image'
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<a href="{}" target="_blank"><img src="{}" style="height:200px;width:200px;object-fit:cover;border-radius:6px;" /></a>',
+                obj.image.url,
+                obj.image.url
+            )
+        return "No image"
+
+    image_preview.short_description = "Preview"
